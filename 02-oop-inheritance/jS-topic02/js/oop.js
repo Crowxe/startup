36e74a987d19e09a795fac68eba74514c6ba0ba0
Crowxe <crowiejose@gmail.com>
2018-03-05 23:27:14 -0300

    Updated

--- 02-oop-inheritance/jS-topic02/js/oop.js
+++ 02-oop-inheritance/jS-topic02/js/oop.js
@@ -1,39 +1,39 @@
-/*https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Classes*/
+/*https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Classes/*/
 
-import Actor from "js/Actor.js"
-import Logger from "js/Logger.js"
-import event_emitter from "js/EventEmitter.js"
-import Movie from "js/Movie.js"
+const Actor = require('./Actor');
+const Logger = require('./Logger');
+const Movie = require('./Movie');
 
-let taxid = new Movie("Interstellar", 2014, "3h 0m");
-
-const terminator = new Movie('Terminator I', 1985, 60);
+let terminator = new Movie('Terminator I', 1985, 60);
 const arnold = new Actor('Arnold Schwarzenegger', 50);
 const actors = [
     new Actor('Paul Winfield', 50),
     new Actor('Michael Biehn', 50),
     new Actor('Linda Hamilton', 50)
 ];
+
+let social = {
+
+    share(friendName) {
+        console.log(friendName + " shares " + this.title);
+    },
+
+    like(friendName) {
+        console.log(friendName + " likes " + this.title);
+    }
+}
+
+Object.assign(Movie.prototype, social);
+
 const logger = new Logger();
 
 terminator.addCast(arnold);
 terminator.addCast(actors);
+
 terminator.on("play", logger.log);
+terminator.on("pause", logger.log);
+terminator.on("resume", logger.log);
+
 terminator.play();
-
-/* MIXIN */
-
-let social = {
-    share(friend_name){
-        console.log(friend_name + " shares " + this.title);
-    },
-    like(friend_name){
-        console.log(friend_name + " likes " + this.title);
-    }
-}
-
-const spun = new Movie("Spun", 2002, "1h 41m");
-
-Object.assign (spun, social);
-
-spun.like("Daniel Schaerer");
+terminator.pause();
+terminator.resume();
